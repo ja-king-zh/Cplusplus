d99c5aafeef9e68d8684a7d80d97b4a2eac91fcc
詹昊 <2352281364@qq.com>
2021-11-24 21:17:17 +0800

    c++学习之旅开始

---
+++ 开启C++学习之旅/源.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+//
+//namespace bit {
+//	//”Ú
+//	int rand = 0;
+//	int Add(int x, int y) {
+//		return x + y;
+//	}
+//	struct Node {
+//		int val;
+//	};
+//}
+//
+//int main() {
+//	bit::Add(1, 2);
+//	struct bit::Node node;
+//	printf("%d", bit::rand);
+//	return 0;
+//}
+
+
+
+#include<iostream>
+//using namespace std;
+int main() {
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
